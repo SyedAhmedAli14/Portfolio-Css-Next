dc090e676573b816efe07d61465801dc8e31b758
SyedAhmedAli14 <160251856+SyedAhmedAli14@users.noreply.github.com>
2024-11-21 22:52:11 +0500

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    // Handle CSS file imports and minify them
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
-  },
-  swcMinify: true,  // Enable SWC minification
+  swcMinify: false,  // Disable minification for debugging
 };
 
 export default nextConfig;
