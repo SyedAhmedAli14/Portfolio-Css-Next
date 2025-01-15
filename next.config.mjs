bd2a3241a2a3dafb8a01c609833a4eaf1bb5c52a
SyedAhmedAli14 <160251856+SyedAhmedAli14@users.noreply.github.com>
2025-01-15 16:57:29 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: false,  // Disable minification for debugging
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true, // Enable React Strict Mode
+  eslint: {
+    // Ignore specific ESLint rules during development and builds
+    ignoreDuringBuilds: true, // Skip ESLint checks during builds
+  },
 };
 
 export default nextConfig;
